652a017734e6653d881c0d44069f84e63e4c2dd1
LeticiaAlmeida16 <leticia.almeidaaa2008@gmail.com>
2024-03-28 09:05:04 -0300

    criei-cor

--- AULA06/IW-ManipulandoCSS/js/script.js
+++ AULA06/IW-ManipulandoCSS/js/script.js
@@ -1,46 +1,90 @@
 /* Arquivo JS + alterar CSS */
 
-//let titulo = document.querySelector('h1')
-//titulo.textContent = 'Aula 09 Manipular CSS'
+let titulo = document.querySelector('h1')
+titulo.textContent = 'Aula 09 Manipular CSS'
 //titulo.innerHTML = 'Aula Manipular CSS'
-//let imagem = document.querySelector('#foto')
-//imagem.setAttribute('src', 'img/pride-fc.jpg')
-//imagem.setAttribute('width', '280px')
+let imagem = document.querySelector('#foto')
+imagem.setAttribute('src', 'img/pride-fc.jpg')
+imagem.setAttribute('width', '280px')
 
 /* MANIPULAR CSS */
-//document.querySelector('h1').style.color = "red";
-//titulo.style.color = "red";
-//titulo.style.backgroundColor = "#000";
-//titulo.style.borderBottom = "2px solid red";
-//titulo.style.padding = "0.625rem";
-//titulo.style.borderRadius = "5px";
+document.querySelector('h1').style.color = "red";
+titulo.style.color = "red";
+titulo.style.backgroundColor = "#000";
+titulo.style.borderBottom = "2px solid red";
+titulo.style.padding = "0.625rem";
+titulo.style.borderRadius = "5px";
 
-//let box = document.querySelectorAll('.box')
-//box[0].setAttribute('class', 'escura')
-//box[0].removeAttribute('class')
+let box = document.querySelectorAll('.box')
+box[0].setAttribute('class', 'escura')
+box[0].removeAttribute('class')
 
 /////// MODOS DE COR ///////
-//let tela = document.querySelector('main')
+let tela = document.querySelector('main')
 
-//let btnDark = document.querySelector('#btdark')
+let btnDark = document.querySelector('#btdark')
 
-//let btnLight = document.querySelector('#btlight')
+let btnLight = document.querySelector('#btlight')
 
-//btnDark.addEventListener('click', modoDark)
-//btnLight.addEventListener('click', modoLight)
+let btnpink = document.querySelector('#btpink')
+
+let btnblue = document.querySelector('#btblue')
+
+let btnyellow = document.querySelector('#btyellow')
+
+btnDark.addEventListener('click', modoDark)
+btnLight.addEventListener('click', modoLight)
+btnpink.addEventListener('click', modopink)
+btnblue.addEventListener('click', modoblue)
+btnyellow.addEventListener('click', modoyellow)
 
 function modoDark() {
-    //event.preventDefault();
+    event.preventDefault();
     console.log('modo dark')
     tela.classList.add("dark");
     tela.classList.remove("light");
+    tela.classList.remove("pink");
+    tela.classList.remove("blue");
+    tela.classList.remove("yellow");
 }
 
 function modoLight() {
-    //event.preventDefault();
+    event.preventDefault();
     console.log('modo light')
     tela.classList.remove("dark");
+    tela.classList.remove("pink");
+    tela.classList.remove("blue");
+    tela.classList.remove("yellow");
     tela.classList.add("light");
+}
+
+function modopink() {
+    event.preventDefault();
+    console.log('modo pink')
+    tela.classList.remove("dark");
+    tela.classList.remove("light");
+    tela.classList.remove("blue");
+    tela.classList.remove("yellow");
+    tela.classList.add("pink");
+}
+
+function modoblue() {
+    event.preventDefault();
+    console.log('modo blue')
+    tela.classList.remove("dark");
+    tela.classList.remove("light");
+    tela.classList.remove("pink");
+    tela.classList.remove("yellow");
+    tela.classList.add("blue");
+}
+function modoyellow() {
+    event.preventDefault();
+    console.log('modo yellow')
+    tela.classList.remove("dark");
+    tela.classList.remove("light");
+    tela.classList.remove("pink");
+    tela.classList.remove("blue");
+    tela.classList.add("yellow");
 }
 
 /*
